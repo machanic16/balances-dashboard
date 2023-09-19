8f6e2770d0fdb0b4c4f64536f4ea2ab278971a5c
Joel Tovar <jatr160397@gmail.com>
2023-09-18 20:41:22 -0400

    ADD view and models
    Display status of the racks to the final user

--- balances_dashboard/dashboard/models.py
+++ balances_dashboard/dashboard/models.py
@@ -18,7 +18,22 @@ class Store(models.Model):
 
     def __str__(self) -> str:
         return self.company.name + '-' + self.name + '\n'
-
+    
+    def statuses(self):
+        ''' Help: 
+                0 : At least one rack is empty so is needed to visit the store
+                1 : At least one rack needs to be filled but is no empty so is not an emergency
+                2 : Some or all racks are good so don't need to be visited
+                3 : All racks are full'''
+        statuses = [rack.status for rack in self.rack_set.all()]
+        if 'empty' in statuses:
+            return 0
+        elif 'needs_refil' in statuses :
+            return 1
+        elif len(set(statuses)) > 1 :
+            return 2
+        else :
+            return 3
 
     
 
@@ -34,7 +49,7 @@ class Rack(models.Model):
     status = models.CharField(max_length=30,choices=status_choices,default='empty')
 
     def __str__(self) -> str:
-        return self.name + '-->' + self.store.name + '\n'
+        return self.name + '-->'+ self.store.company.name +'-'+ self.store.name + '\n'
 
 
 
