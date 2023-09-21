5398c89421ff9ed47ea47b38a497552e8ba15ae4
Joel Tovar <jatr160397@gmail.com>
2023-09-20 22:01:56 -0400

    Update settings.py

--- balances_dashboard/balances_dashboard/settings.py
+++ balances_dashboard/balances_dashboard/settings.py
@@ -143,6 +143,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Add your additional directories here
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
