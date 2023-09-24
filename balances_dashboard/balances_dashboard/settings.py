d9f4ad4c8895cac447c2dbc4bc8e19ab336966c0
Joel Tovar <jatr160397@gmail.com>
2023-09-23 22:01:29 -0400

    d

--- balances_dashboard/balances_dashboard/settings.py
+++ balances_dashboard/balances_dashboard/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# # Additional directories to search for static files
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Add your additional directories here
-# django_heroku.settings(locals())
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Additional directories to search for static files
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Add your additional directories here
+django_heroku.settings(locals())
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
